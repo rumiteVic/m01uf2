1ba148c8587252fbb934e5b8ffdace5513484fa9
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-11-15 09:58:23 +0100

    Implementado el file_name

--- client.sh
+++ client.sh
@@ -6,7 +6,7 @@ echo "Cliente de Dragon Magia Abuelita Miedo 2022"
 echo "1. ENVIO DE CABECERA"
 
 
-echo "DNAM" | nc 127.0.0.1 $PORT
+echo "DMAM" | nc 127.0.0.1 $PORT
 
 DATA=`nc -l $PORT`
 
@@ -15,3 +15,12 @@ then
 	echo "ERROR 1: el header se envio incorrectamente"
 	exit 1
 fi
+
+echo "4. Enviando el FILE_NAME"
+
+FILE_NAME="dragon.txt"
+
+echo "FIPE_NAME $FILE_NAME" | nc localhost $PORT
+
+DATA=`nc -l $PORT`
+
--- server.sh
+++ server.sh
@@ -9,9 +9,26 @@ DATA=`nc -l $PORT`
 if [ "$DATA" != "DMAM" ]
 then
 	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc localhost 2022
+	echo "KO_HEADER" | nc localhost $PORT
 	exit 1
 fi
 
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc localhost 2022
+echo "OK_HEADER" | nc localhost $PORT
+
+echo "5.COMPROBANDO PREFIJO"
+
+DATA=`nc -l $PORT`
+
+PREFIX=`echo "$DATA" | cut -d ' ' -f1`
+
+echo $DATA
+
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "ERROR 2: Prefijo incorrecto"
+	echo "KO_FILE_NAME" | nc localhost 2022
+	exit 2
+fi
+
+echo "OK_FILE_NAME" | nc localhost  2022
