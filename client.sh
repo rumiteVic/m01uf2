4ae5c868b23064dfdd242d809a739e86fae67615
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2024-11-20 11:19:32 +0100

    Mas modificaciones

--- client.sh
+++ client.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+if [ "$1" == "" ]
+then
+	echo "Debes indicar la direccion del servidor."
+	echo "Ejemplo:"
+	echo -e "\t$0 127.0.0.1"
+	exit 1
+fi
+
+IP_Server=$1
+
+IP=`ip a | grep "scope global" | xargs | cut -d " " -f 2 | cut -d "/" -f 1`
 PORT="2022"
 
 echo "Cliente de Dragon Magia Abuelita Miedo 2022"
@@ -6,8 +18,7 @@ echo "Cliente de Dragon Magia Abuelita Miedo 2022"
 echo "1. ENVIO DE CABECERA"
 
 
-echo "DMAM" | nc 127.0.0.1 $PORT
-
+echo "DMAM $IP" | nc $IP $PORT
 DATA=`nc -l $PORT`
 
 if [ "$DATA" != "OK_HEADER" ]
@@ -20,7 +31,7 @@ echo "4. Enviando el FILE_NAME"
 
 FILE_NAME="dragon.txt"
 
-echo "FILE_NAME $FILE_NAME" | nc localhost $PORT
+echo "FILE_NAME $FILE_NAME" | nc $IP $PORT
 
 echo "7. RECIBIENDO OK_FILE_NAME"
 DATA=`nc -l $PORT`
@@ -30,7 +41,7 @@ MD5=`cat ./client/dragon.txt`
 echo $MD5
 ENVIANDO=`echo $MD5 | md5sum`
 echo $ENVIANDO
-echo $ENVIANDO| nc localhost $PORT
+echo $ENVIANDO| nc $IP $PORT
 
 echo "9.RECIBIENDO OK_DRAGON"
 DATA=`nc -l $PORT`
--- server.sh
+++ server.sh
@@ -5,17 +5,20 @@ echo "Servidor de Dragon Magia Abuelita Miedo 2022"
 
 echo "0. escuchamos"
 DATA=`nc -l $PORT`
+HEADER=`echo "$DATA" | cut -d " " -f1`
+IP=`echo "$DATA" | cut -d " " -f2`
 
-if [ "$DATA" != "DMAM" ]
+if [ "$HEADER" != "DMAM" ]
 then
 	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc localhost $PORT
+	echo "KO_HEADER" | nc $IP $PORT
 	exit 1
 fi
 
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc localhost $PORT
+echo "OK_HEADER" | nc $IP $PORT
 
+echo "La IP del cliente es: $IP"
 echo "5.COMPROBANDO PREFIJO"
 
 DATA=`nc -l $PORT`
@@ -27,13 +30,13 @@ echo $DATA
 if [ "$PREFIX" != "FILE_NAME" ]
 then
 	echo "ERROR 2: Prefijo incorrecto"
-	echo "KO_FILE_NAME" | nc localhost $PORT
+	echo "KO_FILE_NAME" | nc $IP $PORT
 	exit 2
 fi
 
 echo "6. ENVIANDO OK_FILE_NAME"
 
-echo "OK_FILE_NAME" | nc localhost $PORT
+echo "OK_FILE_NAME" | nc $IP $PORT
 
 echo "9. RECIBIENDO UN ARCHIVO"
 DATA=`nc -l $PORT`
@@ -46,12 +49,12 @@ echo "10. COMPROBANDO DRAGON"
 if [ "$DATA" != "$COMPROBACION" ]
 then
 	echo "ERROR 3: Dragon incorrecto"
-	echo "KO_DRAGON" | nc localhost $PORT
+	echo "KO_DRAGON" | nc $IP $PORT
 	exit 3
 fi
 
 echo "11. ENVIANDO DRAGON"
-echo "OK_DRAGON" | nc localhost $PORT
+echo "OK_DRAGON" | nc $IP $PORT
 
 echo "12. FIN DRAGON"
 DATA=`nc -l $PORT`
